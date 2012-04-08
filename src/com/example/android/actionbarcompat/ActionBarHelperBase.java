b88be79a6f8b2b0846174a71fdc977da0233201f
Tama Eguchi <tama.eguchi@gmail.com>
2012-04-08 15:03:57 +0900

    不要コード整理

--- src/com/example/android/actionbarcompat/ActionBarHelperBase.java
+++ src/com/example/android/actionbarcompat/ActionBarHelperBase.java
@@ -187,8 +187,7 @@ public class ActionBarHelperBase extends ActionBarHelper {
      * Adds an action button to the compatibility action bar, using menu
      * information from a {@link android.view.MenuItem}. If the menu item ID is
      * <code>menu_refresh</code>, the menu item's state can be changed to show a
-     * loading spinner using
-     * .
+     * loading spinner using .
      */
     private View addActionItemCompatFromMenuItem(final MenuItem item) {
         final int itemId = item.getItemId();
@@ -212,9 +211,6 @@ public class ActionBarHelperBase extends ActionBarHelper {
                                         itemId == android.R.id.home ? R.dimen.actionbar_compat_button_home_width
                                                 : R.dimen.actionbar_compat_button_width),
                         ViewGroup.LayoutParams.MATCH_PARENT));
-        if (itemId == R.id.menu_refresh) {
-            actionButton.setId(R.id.actionbar_compat_item_refresh);
-        }
         actionButton.setImageDrawable(item.getIcon());
         actionButton.setScaleType(ImageView.ScaleType.FIT_CENTER);
         actionButton.setContentDescription(item.getTitle());
@@ -226,32 +222,6 @@ public class ActionBarHelperBase extends ActionBarHelper {
         });
 
         actionBar.addView(actionButton);
-
-        if (item.getItemId() == R.id.menu_refresh) {
-            // Refresh buttons should be stateful, and allow for indeterminate
-            // progress indicators,
-            // so add those.
-            ProgressBar indicator = new ProgressBar(mActivity, null,
-                    R.attr.actionbarCompatProgressIndicatorStyle);
-
-            final int buttonWidth = mActivity.getResources()
-                    .getDimensionPixelSize(
-                            R.dimen.actionbar_compat_button_width);
-            final int buttonHeight = mActivity.getResources()
-                    .getDimensionPixelSize(R.dimen.actionbar_compat_height);
-            final int progressIndicatorWidth = buttonWidth / 2;
-
-            LinearLayout.LayoutParams indicatorLayoutParams = new LinearLayout.LayoutParams(
-                    progressIndicatorWidth, progressIndicatorWidth);
-            indicatorLayoutParams.setMargins(
-                    (buttonWidth - progressIndicatorWidth) / 2,
-                    (buttonHeight - progressIndicatorWidth) / 2,
-                    (buttonWidth - progressIndicatorWidth) / 2, 0);
-            indicator.setLayoutParams(indicatorLayoutParams);
-            indicator.setVisibility(View.GONE);
-            indicator.setId(R.id.actionbar_compat_item_refresh_progress);
-            actionBar.addView(indicator);
-        }
 
         return actionButton;
     }
